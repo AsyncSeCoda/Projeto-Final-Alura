05588e4f833484d6cd8c74a1c66e56ad8d6d040a
Isabela Costa <isa-hcosta@hotmail.com>
2023-06-27 10:11:06 -0300

    adiciona schema de cliente

---
+++ clientes/src/models/Client.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+
+const dadosPessoaisSchema = new mongoose.Schema(
+  {
+    _id: { type: String, auto: false },
+    nome: { type: String },
+    cpf: { type: String },
+    email: { type: String },
+    telefone: { type: String },
+    rendaMensal: { type: Number },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const enderoSchema = new mongoose.Schema(
+  {
+    _id: { type: String, auto: false },
+    rua: { type: String },
+    numero: { type: String || Number },
+    complemento: { type: String || Number },
+    cep: { type: String },
+    cidade: { type: String },
+    estado: { type: String },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const cartaoSchema = new mongoose.Schema(
+  {
+    _id: { type: String, auto: false },
+    numeroCartao: { type: String },
+    nomeCartao: { type: String },
+    validadeCartao: { type: Date },
+    cvcCartao: { type: String },
+    vencimentoFatura: { type: Date },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const clientSchema = new mongoose.Schema(
+  {
+    dadosPessoais: { type: dadosPessoaisSchema, required: true },
+    endereco: { type: enderoSchema, required: true },
+    cartao: { type: cartaoSchema, required: true },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const Client = mongoose.model('clients', clientSchema);
+
+export default Client;
