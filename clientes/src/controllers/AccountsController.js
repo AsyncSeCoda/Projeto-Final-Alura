1e65e469085f04ccada035b43053070491096293
Isabela Costa <isa-hcosta@hotmail.com>
2023-06-27 18:03:31 -0300

    retira um try/catch

--- clientes/src/controllers/AccountsController.js
+++ clientes/src/controllers/AccountsController.js
@@ -27,34 +27,28 @@ class AccountController {
     });
   };
 
-  static createAccount = async (req, res, next) => {
+  static createAccount = async (req, res) => {
     const { nome, email, senha } = req.body;
 
+    const conta = await Account.findOne({ email });
+    if (conta) {
+      return res.status(409).json({ message: 'Já existe uma conta com esse email' });
+    }
+    const salt = await bcrypt.genSalt(12);
+    const senhaHash = await bcrypt.hash(senha, salt);
+
+    const account = new Account({
+      nome,
+      email,
+      senha: senhaHash,
+      createdDate: Date(),
+    });
     try {
-      const conta = await Account.findOne({ email });
-      if (conta) {
-        return res.status(409).json({ message: 'Já existe uma conta com esse email' });
-      }
-      const salt = await bcrypt.genSalt(12);
-      const senhaHash = await bcrypt.hash(senha, salt);
-
-      const account = new Account({
-        nome,
-        email,
-        senha: senhaHash,
-        createdDate: Date(),
-      });
-
-      account.save((err, newAccount) => {
-        if (err) {
-          return res.status(500).send({ message: err.message });
-        }
-        return res.status(201).set('Location', `/admin/accounts/${account.id}`).json(newAccount);
-      });
+      await account.save();
+      return res.status(201).set('Location', `/admin/accounts/${account.id}`).json(account);
     } catch (err) {
       return res.status(500).send({ message: err.message });
     }
-    return next();
   };
 
   static updateAccount = (req, res) => {
