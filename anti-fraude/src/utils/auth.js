6fffe555936ff96aa869a8ef39cae5950e9d11a8
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-06-28 14:29:27 -0300

    Protegendo a senha do usuário com bcrypt

---
+++ anti-fraude/src/utils/auth.js
@@ -0,0 +1,72 @@
+/* eslint-disable linebreak-style */
+import dotenv from 'dotenv';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as BearerStrategy } from 'passport-http-bearer';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import Account from '../models/Account.js';
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+async function buscaPorId(id) {
+  const usuario = Account.findById(id);
+  if (!usuario) {
+    return null;
+  }
+
+  return new Account(usuario);
+}
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'senha',
+      session: 'false',
+    },
+    async (email, senha, done) => {
+      try {
+        const account = await Account.findOne({ email });
+
+        if (!account) {
+          return done(null, false, { message: 'Email ou senha inválido' });
+        }
+
+        const isMatch = await bcrypt.compare(senha, account.senha);
+
+        if (!isMatch) {
+          return done(null, false, { message: 'Email ou senha inválido' });
+        }
+
+        return done(null, account);
+      } catch (err) {
+        return done(err);
+      }
+    },
+  ),
+);
+
+passport.use(
+  new BearerStrategy(
+    async (token, done) => {
+      try {
+        const payload = jwt.verify(token, JWT_SECRET);
+        const account = await buscaPorId(payload.id);
+        done(null, account);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
+);
+
+const generateToken = (id) => {
+  const payload = { id };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' });
+  return token;
+};
+
+export default generateToken;
---
+++ anti-fraude/src/utils/middleware.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+import passport from 'passport';
+
+function local(req, res, next) {
+  passport.authenticate(
+    'local',
+    { session: false },
+    (erro, usuario) => {
+      if (erro) {
+        return res.status(500).json({ erro: erro.message });
+      }
+
+      if (!usuario) {
+        return res.status(401).json();
+      }
+
+      req.user = usuario;
+      return next();
+    },
+  )(req, res, next);
+}
+
+function bearer(req, res, next) {
+  passport.authenticate(
+    'bearer',
+    { session: false },
+    (erro, usuario) => {
+      if (erro) {
+        return res.status(500).json({ erro: erro.message });
+      }
+
+      if (!usuario) {
+        return res.status(401).json();
+      }
+
+      req.user = usuario;
+      return next();
+    },
+  )(req, res, next);
+}
+
+export { local, bearer };
