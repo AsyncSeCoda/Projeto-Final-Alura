db9f9996cc7df608adc56b73a8e659083adc5edb
Marina Pilatti <marina.campospilatti03@gmail.com>
2023-06-29 21:40:55 -0300

    findbyid na anti-fraud

--- anti-fraude/src/controllers/AntiFraudController.js
+++ anti-fraude/src/controllers/AntiFraudController.js
@@ -1,29 +1,37 @@
 import AntiFraud from '../models/AntiFraude.js';
+import axios from 'axios';
 
-function determinaValorAnalise(status) {
-  if (status === 'Em Análise') {
-    const valorStatus = 'Aprovada' || 'Reprovada';
-    return { status: valorStatus };
+function determinaStatus(statusAntigo, statusUpdate) {
+  const valorStatus = 'Aprovada' || 'Reprovada';
+  if (statusAntigo === 'Em Análise') {
+    return valorStatus;
   }
-  return { status };
+  //else if (statusAntigo === valorStatus) {
+//     return { status: valorStatus }; 
+//   }
+  return statusUpdate;
 }
 
 class antiFraudController {
   static findAntiFraudInAnalysis = async (req, res) => {
     try {
-      const response = await AntiFraud.findOne({ status: 'Em análise' }).exec();
+      const response = await AntiFraud.find({ status: 'Em análise' }).exec();
       res.status(200).json(response);
-    } catch {
+    } catch (err) {
       res.status(404).send('Nenhum caso encontrado');
     }
   };
 
+
   static findAntiFraudById = async (req, res) => {
     const { id } = req.params;
     try {
-      const response = await AntiFraud.findById({ _id: id }).populate('idCliente').populate('idTransicao').exec();
-      res.status(200).json(response);
-    } catch {
+      const response = await AntiFraud.findById(id);
+      const responseClient = await axios.get(`http://127.0.0.1:3006/api/admin/clients/${response.idCliente}`);
+      const responseTransaction = await axios.get(`http://127.0.0.1:3005/api/admin/transactions/${response.idTransacao}`);
+      const valor = responseTransaction.data.valor;
+      res.status(200).json({...responseClient.data, ...response._doc, valor});
+    } catch (err) {
       res.status(404).send('Nenhum caso encontrado');
     }
   };
@@ -41,9 +49,27 @@ class antiFraudController {
   static updateAntiFraud = async (req, res) => {
     const { id } = req.params;
     try {
-      await AntiFraud.findByIdAndUpdate({ _id: id }, determinaValorAnalise(req.body.status));
-      res.status(200).send('Caso atualizado');
-    } catch {
+      const response = await AntiFraud.findById(id).exec();
+
+      //response é o documento não atualizado
+      //a função determina Status pega o valor antigo de status e faz uma verificação 
+      const valorUpdate = determinaStatus(response.status, req.body.status);
+      console.log(valorUpdate);
+
+      AntiFraud.findByIdAndUpdate(id, { status: valorUpdate });
+
+      //const responseTransaction = await fetch('http://localhost:3001/api/admin/transcation/:id');
+
+    // if (response.status === 404) throw new Error(await response.text());
+
+    // const responseBody = await response.json();
+
+    // if (response.status === 400) throw new Error(responseBody.errorMessage);
+
+    // return responseBody;
+    res.status(200).send('Caso não encontrado');
+
+    } catch (err) {
       res.status(404).send('Caso não encontrado');
     }
   };
